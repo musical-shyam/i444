54838fd9e7de32c8c22cf5f9438e7e46196a0de2
musical-shyam <skannan@binghamton.edu>
2024-03-13 08:40:52 -0400

    completed library.ts

--- submit/prj2-sol/src/lib/library.ts
+++ submit/prj2-sol/src/lib/library.ts
@@ -23,14 +23,15 @@ const MSGS = {
 //   year: an integer within the range [GUTENBERG_YEAR, NOW_YEAR].
 //   publisher: a non-empty string.
 //   nCopies: an optional positive integer
-const Book =  z.object({
-  isbn: z.string(),
-  title: z.string(),
-  authors: z.string().array(),
-  pages: z.number(),
-  year: z.number(),
-  publisher: z.string(),
-  nCopies: z.number(),
+
+const Book = z.object({
+  isbn: z.string().regex(/^\d{3}-\d{3}-\d{3}-\d{1}$/),
+  title: z.string().min(1),
+  authors: z.array(z.string().min(1)).nonempty(),
+  pages: z.number().positive().int(),
+  year: z.number().int().gte(GUTENBERG_YEAR).lte(NOW_YEAR),
+  publisher: z.string().min(1),
+  nCopies: z.number().positive().int().optional(),
 });
 
 export type Book = z.infer<typeof Book>;
@@ -43,18 +44,19 @@ export type XBook = z.infer<typeof XBook>;
 //   index: an optional non-negative integer.
 //   count: an optional non-negative integer.
 const Find = z.object({
-  search: z.string(),
-  index: z.number(),
-  count: z.number()
+  search: z.string().regex(/(\w{2,})/),
+  index: z.number().nonnegative().optional(),
+  count: z.number().nonnegative().optional(),
 });
+
 export type Find = z.infer<typeof Find>;
 
 // use zod to force Lend to have the following fields:
 //   isbn: a ISBN-10 string of the form ddd-ddd-ddd-d.
 //   patronId: a non-empty string.
 const Lend = z.object({
-  isbn: z.string(),
-  patronId: z.string(),
+  isbn: z.string().regex(/^\d{3}-\d{3}-\d{3}-\d{1}$/),
+  patronId: z.string().min(1),
 });
 export type Lend = z.infer<typeof Lend>;
 
